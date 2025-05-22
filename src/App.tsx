67ab22e7c626d9577e210303f22dffc7480f4b8e
Meenaalhasani <Meenaalhasani1@gmail.com>
2025-05-22 15:54:16 -0500

    updated homepage layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <header>
+        <h1>Meena Al Hasani</h1>
+        <p>Software Engineer</p>
+      </header>
+
+      <section id="About me">
+          <h2>About Me</h2>
+          <p>Fill later</p>
+      </section>
+
+      <section id="Projects">
+        <h2>Projects</h2>
+        <p>fill later</p>
+      </section>
+
+      <section id="Resume">
+        <h2>Resume</h2>
+        <a href="Meena Resume.pdf" download>Download PDF</a>
+      </section>
+
+      <section id="Contact">
+        <h2>Contact</h2>
+        <p>Email: meenaalhasani1@gmail.com</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <a href="https://linkedin.com/in/meenaalhasani">LinkedIn</a>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </section>
+    </div>
+  );
 }
 
 export default App
